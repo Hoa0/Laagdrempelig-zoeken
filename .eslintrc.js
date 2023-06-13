48c28474839528c3a7b0ac1fbd98e91d1914daab
hoa0 <t-hoa.l@hotmail.com>
2023-06-13 09:52:52 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,11 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "camelcase": ["error", { "properties": "always" }],
+        "block-spacing": ["error", "always"],
+        "comma-spacing": ["error", { "before": false, "after": true }],
+        "no-mixed-spaces-and-tabs": ["error", "smart-tabs"]
     }
 }
