1731bd08da3dc2af8bef8fb12e42b29c65d313d3
AllyssaA <a.alimoestar@gmail.com>
2023-06-13 16:40:42 +0200

    :sparkles: fetch data oba api

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // Set the port for the server to listen on
 const port = process.env.PORT || 3002;
 
+const apiRoutes = require('./routes/routes');
+
 // Set up middleware to serve static files
+// Parse JSON bodies
+app.use(bodyParser.json());
 app.use(express.static(path.resolve('public')));
+app.use('/api', apiRoutes);
 
 // Set the view engine to ejs and set the views directory
 app.set('view engine', 'ejs');
