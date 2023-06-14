a59e9937f82fc4ea7d2bdf5f1834b596ae0f5f75
hoa0 <t-hoa.l@hotmail.com>
2023-06-14 12:57:52 +0200

    update

--- public/js/script.js
+++ public/js/script.js
@@ -4,14 +4,15 @@ const texts = document.getElementsByClassName("showResult");
 const content = document.querySelector("#content");
 const obaHelper = document.getElementById("svg-help");
 const contentChat = document.getElementById("chat");
-const result = document.getElementById("searchResult");
+
+
 
 
 button.addEventListener("click", function () {
     for (let i = 0; i < texts.length; i++) {
         texts[i].style.display = "block";
     }
-    gsap.from(result, { duration: 1, y: 30, opacity: 0 });
+    gsap.from(texts, { duration: 1, y: 30, opacity: 0 });
 });
 
 function animation() {
