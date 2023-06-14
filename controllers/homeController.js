8753a15fe85fac90d36b4fe32c56fb06bbafd645
AllyssaA <a.alimoestar@gmail.com>
2023-06-14 13:08:57 +0200

    :recycle: make use of imports

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,35 +1,34 @@
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
-const fetchData = async (req, res) => {
+export const fetchData = async (req, res) => {
   const publicKey = "0076bc3bc11d080e07a303360178002a";
-  const query = req.body.query; 
+  const query = req.body.query;
 
-  const apiUrl = "https://zoeken.oba.nl/api/v1/search/?q=" + query;;
+  const apiUrl = "https://zoeken.oba.nl/api/v1/search/?q=" + query;
   const publicKeyParam = `&authorization=${publicKey}`;
   const outputParam = "&output=json";
 
   const apiUrlWithParams = apiUrl + publicKeyParam + outputParam;
-  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
   const corsApiUrl = corsProxy + apiUrlWithParams;
-  console.log(corsApiUrl)
+  console.log(corsApiUrl);
 
   try {
     // Make the API request using fetch
     const response = await fetch(corsApiUrl, {
       headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        
+        "X-Requested-With": "XMLHttpRequest",
       },
     });
 
     if (!response.ok) {
-        // Throw an error if the response is not successful
+      // Throw an error if the response is not successful
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     // Parse the response body as text
     const responseBody = await response.text();
     // console.log(responseBody); // Log the fetched data to the console
-    
+
     // Render the response in the index.ejs template
     // res.render("pages/index.ejs", { responseBody });
     res.json({ data: responseBody });
@@ -37,8 +36,4 @@ const fetchData = async (req, res) => {
     console.error("Error:", error.message);
     res.status(500).json({ error: "An error occurred" });
   }
-};
-
-module.exports = {
-  fetchData
 };
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
+import express from 'express';
+import { createServer } from 'http';
+import path from 'path';
+import bodyParser from 'body-parser';
+import router from './routes/route.js';
+
 const app = express();
-const http = require('http').createServer(app);
-const path = require('path');
-const bodyParser = require('body-parser');
+const http = createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Set the port for the server to listen on
 const port = process.env.PORT || 3002;
 
-const apiRoutes = require('./routes/routes');
 
 // Set up middleware to serve static files
 // Parse JSON bodies
-app.use(bodyParser.json());
 app.use(express.static(path.resolve('public')));
-app.use('/api', apiRoutes);
+app.use('/api', router);
 
 // Set the view engine to ejs and set the views directory
 app.set('view engine', 'ejs');
