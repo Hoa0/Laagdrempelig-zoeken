da9ece6a6974b3c83d6bfb2b28ad9e8ea916b1c6
AllyssaA <a.alimoestar@gmail.com>
2023-06-14 13:22:02 +0200

    :rotating_light::rotating_light:

--- public/js/script.js
+++ public/js/script.js
@@ -1,67 +1,66 @@
-const searchForm = document.getElementById('searchForm');
-const searchResults = document.getElementById('searchResults'); // Add this line
+const searchForm = document.getElementById("searchForm");
+const searchResults = document.getElementById("searchResults"); // Add this line
 
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault(); // Prevent default form submission
+searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent default form submission
 
-    const input = document.getElementById('query');
+  const input = document.getElementById("query");
 
-    try {
-        const response = await fetch('/api/search', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ query: input.value }),
-        });
+  try {
+    const response = await fetch("/api/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: input.value }),
+    });
 
-        if (response.ok) {
-            const responseData = await response.json();
+    if (response.ok) {
+      const responseData = await response.json();
 
-            // console.log(responseData.data)
-            // let crackdown = JSON.parse(JSON.stringify(responseData.data))
-            // let firstRemoval = crackdown.substring(1)
+      // console.log(responseData.data)
+      // let crackdown = JSON.parse(JSON.stringify(responseData.data))
+      // let firstRemoval = crackdown.substring(1)
 
-            const dirtySet = JSON.parse(JSON.stringify(responseData.data))
-            // const cleanSet = dirtySet.substring(1)
+      const dirtySet = JSON.parse(JSON.stringify(responseData.data));
+      // const cleanSet = dirtySet.substring(1)
 
-            // Show books in the browser using responseData
-            // Clear previous results
-            searchResults.innerHTML = '';
+      // Show books in the browser using responseData
+      // Clear previous results
+      searchResults.innerHTML = "";
 
+      if (await responseData.data) {
+        // let x = await responseData
+        // let remResponseData = JSON.parse(firstRemoval)
 
-            if (await responseData.data) {
-                // let x = await responseData
-                // let remResponseData = JSON.parse(firstRemoval)
-
-                /** @description removes the first string character. Reason for the function is that the dataset received is being tossed around between stringify and parse, adding an empty character within the body of the results key */
-                function RemoveSpaceFromString(dataThatHasBeenStringified) {
-                    return dataThatHasBeenStringified.substring(1)
-                }
-                let responseDataSet = JSON.parse(RemoveSpaceFromString(dirtySet))
-                console.log(responseDataSet)
-
-                // Display the search results
-                responseDataSet.results.forEach((result) => {
-
-                    const title = result.titles[0];
-                    const author = result.authors ? result.authors[0] : "Onbekende auteur";
-                    const img = result.coverimages[0];
-            
-                    const resultItem = document.createElement('div');
-                    resultItem.innerHTML = `<p>Titel: ${title}</p><p>Auteur: ${author}</p> <img src='${img}'>`;
-            
-                    searchResults.appendChild(resultItem);
-                });
-            } else {
-                console.log('No results found.');
-            }
-            
-        } else {
-            const errorMessage = await response.text();
-            console.error(errorMessage);
+        /** @description removes the first string character. Reason for the function is that the dataset received is being tossed around between stringify and parse, adding an empty character within the body of the results key */
+        function RemoveSpaceFromString(dataThatHasBeenStringified) {
+          return dataThatHasBeenStringified.substring(1);
         }
-    } catch (error) {
-        console.error('Error:', error.message);
+        let responseDataSet = JSON.parse(RemoveSpaceFromString(dirtySet));
+        console.log(responseDataSet);
+
+        // Display the search results
+        responseDataSet.results.forEach((result) => {
+          const title = result.titles[0];
+          const author = result.authors
+            ? result.authors[0]
+            : "Onbekende auteur";
+          const img = result.coverimages[0];
+
+          const resultItem = document.createElement("div");
+          resultItem.innerHTML = `<p>Titel: ${title}</p><p>Auteur: ${author}</p> <img src='${img}'>`;
+
+          searchResults.appendChild(resultItem);
+        });
+      } else {
+        console.log("No results found.");
+      }
+    } else {
+      const errorMessage = await response.text();
+      console.error(errorMessage);
     }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { createServer } from 'http';
-import path from 'path';
-import bodyParser from 'body-parser';
-import router from './routes/route.js';
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import bodyParser from "body-parser";
+import router from "./routes/route.js";
 
 const app = express();
 const http = createServer(app);
@@ -13,21 +13,20 @@ app.use(bodyParser.json());
 // Set the port for the server to listen on
 const port = process.env.PORT || 3002;
 
-
 // Set up middleware to serve static files
 // Parse JSON bodies
-app.use(express.static(path.resolve('public')));
-app.use('/api', router);
+app.use(express.static(path.resolve("public")));
+app.use("/api", router);
 
 // Set the view engine to ejs and set the views directory
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Set up a route
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
 http.listen(port, () => {
-    console.log(`listening on port http://localhost:${port}`);
+  console.log(`listening on port http://localhost:${port}`);
 });
