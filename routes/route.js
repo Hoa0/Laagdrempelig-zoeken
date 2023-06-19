a5b398a196d0c1c28dbd3eff6829b425278b4522
AllyssaA <a.alimoestar@gmail.com>
2023-06-19 17:23:52 +0200

    :bento: placeholder img

--- routes/route.js
+++ routes/route.js
@@ -3,18 +3,6 @@ import { fetchData } from '../controllers/homeController.js';
 
 const router = express.Router();
 
-// const apiController = require('../controllers/apiController');
-
-// in /api/example
-// router
-//     .route('/example/')
-//     .get(apiController.getExample)
-//     .post(apiController.postExample);
-
-// .post(object.function-in-the-object)
-// router.route('/search/').post(apiController.search);
-
-// GET route
 router.get('/', fetchData);
 
 router.post('/search', fetchData);
