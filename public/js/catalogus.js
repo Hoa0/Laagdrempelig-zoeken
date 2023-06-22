2e20fc6ce882427da20601c3fa82dbffc6fe5083
hoa0 <t-hoa.l@hotmail.com>
2023-06-22 15:26:17 +0200

    update

--- public/js/catalogus.js
+++ public/js/catalogus.js
@@ -1,6 +1,4 @@
-// public/js/catalogus.js
 const searchButtons = document.querySelectorAll(".searchButton");
-
 searchButtons.forEach((button) => {
     button.addEventListener("click", async () => {
         const category = button.dataset.category;
@@ -14,15 +12,31 @@ searchButtons.forEach((button) => {
         if (results.length > 0) {
             showResults(category, results);
             document.getElementById(category + "Container").style.display = "block";
+
+            // Voeg de titel (h3) toe aan de category-container
+            const containerTitle = document.createElement("h3");
+            containerTitle.textContent = category;
+            document.getElementById(category + "Container").prepend(containerTitle);
+
+            // Geef de reden waarom de gebruiker heeft geklikt weer in de chat
+            const chatMessagesUser = document.getElementById("chatListUser");
+            const chatMessage = document.createElement("li");
+            chatMessagesUser.style.display = "block";
+            chatMessage.className = "chat-message user";
+            chatMessage.innerHTML = '<span>Ik wil informatie vinden over: ' + category + "</span>";
+            chatMessagesUser.appendChild(chatMessage);
         } else {
             document.getElementById(category + "Container").style.display = "none";
             console.log("No results found");
         }
     });
+    // chatMessagesUl.scrollTop = chatMessagesUl.scrollHeight;
 });
 
 function getCategoryFacet(category) {
-    const foundCategory = categories.find((catalogus) => catalogus.name === category);
+    const foundCategory = categories.find(
+        (catalogus) => catalogus.name === category
+    );
     return foundCategory ? foundCategory.facet : "";
 }
 
@@ -37,7 +51,7 @@ function showResults(category, results) {
         if (result.coverimages && result.coverimages[1]) {
             imageUrl = result.coverimages[1];
         } else if (result.coverimages && result.coverimages[0]) {
-            imageUrl = result.coverimages[0].replace("{0}", "l");
+            imageUrl = result.coverimages[0].replace("{0}", "1");
         } else {
             imageUrl = "fallback.png";
         }
@@ -49,26 +63,25 @@ function showResults(category, results) {
             this.src = "fallback.png";
         };
 
-        let detailLink = result.detailLink;
-        if (!detailLink) {
-            detailLink = result.detaillink;
-        }
+        const title = document.createElement("p");
+        title.textContent = result.titles[0];
 
-        const link = document.createElement("a");
-        link.href = detailLink.replace("http:", "https:");
-        link.target = "_blank";
-        link.appendChild(img);
+        const author = document.createElement("p");
+        author.textContent = result.authors ? result.authors[0] : "Unknown Author";
 
         const item = document.createElement("div");
         item.className = "result-item";
-        item.appendChild(link);
+        item.appendChild(title);
+        item.appendChild(author);
+        item.appendChild(img);
 
         resultsContainer.appendChild(item);
     });
 }
+
 let categories = [
     { name: "boeken", facet: "&facet=type(book)&refine=true" },
     { name: "dvds", facet: "&facet=type(movie)&refine=true" },
     { name: "activiteiten", facet: "%20table:Activiteiten&refine=true" },
     { name: "cursussen", facet: "%20table:jsonsrc&refine=true" },
-]
+];
