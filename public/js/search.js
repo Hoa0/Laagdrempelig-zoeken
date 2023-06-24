af1bfceb551ddc22e5b12260d962ca935931d8f4
AllyssaA <a.alimoestar@gmail.com>
2023-06-24 16:21:04 +0200

    :sparkles: added load more button

--- public/js/search.js
+++ public/js/search.js
@@ -1,17 +1,19 @@
 import { uiState } from "./ui.js";
 
 const searchForm = document.getElementById("searchForm");
-// Create the chatMessages element dynamically
 const chatMessages = document.createElement("ul");
 chatMessages.id = "chatMessages";
+const searchResults = document.getElementById("searchResults");
+const itemDetailsDiv = document.getElementById("itemDetails");
+const loadMoreButton = document.getElementById("loadMoreButton");
+
+let currentIndex = 0;
+const itemsPerLoad = 5;
+let responseDataSet;
 
 searchForm.addEventListener("submit", async (event) => {
-  event.preventDefault(); // Prevent default form submission
-  // TODO: scrolltop + scroll height
-  // const chatContainer = document.getElementById("chat");
-  // chatContainer.scrollTop = chatContainer.scrollHeight;
-  const searchResults = document.getElementById("searchResults");
-  const itemDetailsDiv = document.getElementById("itemDetails");
+  event.preventDefault();
+  currentIndex = 0;
   const input = document.getElementById("query");
   const userInput = input.value;
 
@@ -21,7 +23,6 @@ searchForm.addEventListener("submit", async (event) => {
   chatMessages.appendChild(message);
 
   const resultsContainer = document.querySelector(".results-container");
-  // Append the chatMessages element to the resultsContainer
   resultsContainer.appendChild(chatMessages);
 
   try {
@@ -36,21 +37,28 @@ searchForm.addEventListener("submit", async (event) => {
 
     if (response.ok) {
       const responseData = await response.json();
-
       const dirtySet = JSON.parse(JSON.stringify(responseData.data));
-
       searchResults.innerHTML = "";
 
       if (await responseData.data) {
-        /** @description removes the first string character. Reason for the function is that the dataset received is being tossed around between stringify and parse, adding an empty character within the body of the results key */
         function RemoveSpaceFromString(dataThatHasBeenStringified) {
           return dataThatHasBeenStringified.substring(1);
         }
-        let responseDataSet = JSON.parse(RemoveSpaceFromString(dirtySet));
-        console.log(responseDataSet);
+        responseDataSet = JSON.parse(
+          RemoveSpaceFromString(dirtySet)
+        );
 
-        // Display the search results
-        responseDataSet.results.forEach((result) => {
+        localStorage.setItem(
+          "responseDataSet",
+          JSON.stringify(responseDataSet)
+        );
+
+        const itemsToLoad = responseDataSet.results.slice(
+          currentIndex,
+          currentIndex + itemsPerLoad
+        );
+
+        itemsToLoad.forEach((result) => {
           const title =
             result.titles && result.titles.length > 0
               ? result.titles[0]
@@ -72,8 +80,6 @@ searchForm.addEventListener("submit", async (event) => {
 
           const resultItem = document.createElement("div");
           resultItem.innerHTML = `<img src='${img}'> <p>${title}</p><p>${author}</p>`;
-          // Append the result item to the search results
-          searchResults.appendChild(resultItem);
 
           resultItem.addEventListener("click", () => {
             const itemDetails = [
@@ -84,9 +90,9 @@ searchForm.addEventListener("submit", async (event) => {
               { text: `Talen: ${language}` },
               { text: `Uitgever: ${publisher}` },
             ];
-          
+
             const detailsDiv = document.createElement("div");
-          
+
             itemDetails.forEach((item) => {
               if (item.img) {
                 const imgElement = document.createElement("img");
@@ -98,13 +104,21 @@ searchForm.addEventListener("submit", async (event) => {
                 detailsDiv.appendChild(p);
               }
             });
-          
+
             itemDetailsDiv.appendChild(detailsDiv);
-          })          
+          });
 
           searchResults.appendChild(resultItem);
-          // resultsContainer.scrollTop = resultsContainer.scrollHeight;
         });
+
+        currentIndex += itemsPerLoad;
+
+        if (currentIndex >= responseDataSet.results.length) {
+          loadMoreButton.style.display = "none";
+        } else {
+          loadMoreButton.style.display = "block";
+        }
+
         uiState("loading", input.value);
       } else {
         console.log("No results found.");
@@ -118,5 +132,77 @@ searchForm.addEventListener("submit", async (event) => {
     console.error("Error:", error.message);
   } finally {
     uiState("hide", input.value);
+  }
+});
+
+loadMoreButton.addEventListener("click", () => {
+  const storedDataSet = localStorage.getItem("responseDataSet");
+
+  if (storedDataSet) {
+    responseDataSet = JSON.parse(storedDataSet);
+  }
+
+  const itemsToLoad = responseDataSet.results.slice(
+    currentIndex,
+    currentIndex + itemsPerLoad
+  );
+
+  itemsToLoad.forEach((result) => {
+    const title =
+      result.titles && result.titles.length > 0
+        ? result.titles[0]
+        : "Onbekende titel";
+    const author =
+      result.authors && result.authors.length > 0
+        ? result.authors[0]
+        : "Onbekende auteur";
+    const img =
+      result.coverimages[1] !== undefined
+        ? result.coverimages[1]
+        : "./img/placeholder.png";
+    const language = result.languages;
+    const publisher = result.publisher;
+    const summaries =
+      result.summaries && result.summaries.length > 0
+        ? result.summaries[0]
+        : "Niet beschikbaar";
+
+    const resultItem = document.createElement("div");
+    resultItem.innerHTML = `<img src='${img}'> <p>${title}</p><p>${author}</p>`;
+
+    resultItem.addEventListener("click", () => {
+      const itemDetails = [
+        { img: img },
+        { text: `${title}` },
+        { text: `${author}` },
+        { text: `Samenvatting: ${summaries}` },
+        { text: `Talen: ${language}` },
+        { text: `Uitgever: ${publisher}` },
+      ];
+
+      const detailsDiv = document.createElement("div");
+
+      itemDetails.forEach((item) => {
+        if (item.img) {
+          const imgElement = document.createElement("img");
+          imgElement.src = item.img;
+          detailsDiv.appendChild(imgElement);
+        } else {
+          const p = document.createElement("p");
+          p.textContent = item.text;
+          detailsDiv.appendChild(p);
+        }
+      });
+
+      itemDetailsDiv.appendChild(detailsDiv);
+    });
+
+    searchResults.appendChild(resultItem);
+  });
+
+  currentIndex += itemsPerLoad;
+
+  if (currentIndex >= responseDataSet.results.length) {
+    loadMoreButton.style.display = "none";
   }
 });
