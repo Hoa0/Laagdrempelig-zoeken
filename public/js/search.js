a7a977c92ac7b97fd6627a0c11e1ca1b4c04ff52
Hoa0 <49392248+Hoa0@users.noreply.github.com>
2023-06-27 16:54:03 +0200

    Revert "Merge branch '13-refactor-code-es6' of https://github.com/Hoa0/Laagdrempelig-zoeken into 13-refactor-search-result"
    This reverts commit 411145465908446a0ab080fa2011b7a136c1e3a5, reversing
    changes made to f86fed92856e79b563e4f3d2de42f15795a90eac.

--- public/js/search.js
+++ public/js/search.js
@@ -1,16 +1,5 @@
 import { uiState } from "./ui.js";
 
-// Function to set tabindex to 0 for all elements
-const setTabindexToZero = () => {
-  const elements = document.querySelectorAll("*");
-  elements.forEach((element) => {
-    element.setAttribute("tabindex", "0");
-  });
-};
-
-// Call the function to set tabindex to 0
-setTabindexToZero();
-
 // Get DOM elements
 const searchForm = document.getElementById("searchForm");
 const chatMessages = document.createElement("ul");
@@ -35,31 +24,18 @@ const createMessage = (text) => {
 // Function to create a search result item element
 const createResultItem = (result) => {
   // Destructure properties from the result object
-  const {
-    titles,
-    authors,
-    coverimages,
-    languages,
-    publisher,
-    summaries,
-    detailLink,
-  } = result;
+  const { titles, authors, coverimages, languages, publisher, summaries } = result;
 
   // Assign default values if properties are undefined or empty
   const title = titles && titles.length > 0 ? titles[0] : "Onbekende titel";
-  const author =
-    authors && authors.length > 0 ? authors[0] : "Onbekende auteur";
-  const img =
-    coverimages[1] !== undefined ? coverimages[1] : "./img/placeholder.png";
+  const author = authors && authors.length > 0 ? authors[0] : "Onbekende auteur";
+  const img = coverimages[1] !== undefined ? coverimages[1] : "./img/placeholder.png";
   const language = languages;
-  const summariesText =
-    summaries && summaries.length > 0 ? summaries[0] : "Niet beschikbaar";
-  const detailLinks = detailLink;
+  const summariesText = summaries && summaries.length > 0 ? summaries[0] : "Niet beschikbaar";
 
   // Create the result item element
   const resultItem = document.createElement("article");
   resultItem.innerHTML = `<img src='${img}'> <h2>${title}</h2><h3>${author}</h3>`;
-  resultItem.setAttribute("tabindex", "0"); // Add tabindex attribute for tab navigation
 
   // Add click event listener to show item details
   resultItem.addEventListener("click", () => {
@@ -70,7 +46,6 @@ const createResultItem = (result) => {
       { text: `Samenvatting: ${summariesText}` },
       { text: `Talen: ${language}` },
       { text: `Uitgever: ${publisher}` },
-      { text: "Delen", link: detailLinks },
     ];
 
     const detailsDiv = document.createElement("article");
@@ -80,51 +55,23 @@ const createResultItem = (result) => {
         const imgElement = document.createElement("img");
         imgElement.src = item.img;
         detailsDiv.appendChild(imgElement);
-        detailsDiv.setAttribute("tabindex", "0");
-      } else if (item.link) {
-        const button = document.createElement("button");
-        button.innerHTML = `${item.text}`;
-        button.setAttribute("title", "Klik om te delen!");
-        button.innerHTML += `<i class="material-icons">&#xe80d;</i>`;
-        button.addEventListener("click", () => {
-          navigator.clipboard
-            .writeText(item.link)
-            .then(() => {
-              console.log("Link copied to clipboard:", item.link);
-              // Provide user feedback or perform other actions after successful copy
-              uiState("copied");
-            })
-            .catch((error) => {
-              console.error("Failed to copy link:", error);
-              // Handle the error or provide user feedback
-              uiState("noLink");
-            });
-
-          // Scroll to the bottom of the "chat" element
-          const chatElement = document.getElementById("chat");
-          chatElement.scrollTop = chatElement.scrollHeight;
-        });
-        button.setAttribute("tabindex", "0"); // Add tabindex attribute for tab navigation
-        detailsDiv.appendChild(button);
       } else {
         if (index === 1) {
           const h2 = document.createElement("h2");
           h2.textContent = item.text;
-          h2.setAttribute("tabindex", "0"); // Add tabindex attribute for tab navigation
           detailsDiv.appendChild(h2);
         } else if (index === 2) {
           const h3 = document.createElement("h3");
           h3.textContent = item.text;
-          h3.setAttribute("tabindex", "0"); // Add tabindex attribute for tab navigation
           detailsDiv.appendChild(h3);
         } else {
           const p = document.createElement("p");
           p.textContent = item.text;
-          p.setAttribute("tabindex", "0"); // Add tabindex attribute for tab navigation
           detailsDiv.appendChild(p);
         }
       }
     });
+
     itemDetailsDiv.appendChild(detailsDiv);
   });
 
@@ -138,15 +85,13 @@ const loadItems = () => {
     currentIndex + itemsPerLoad
   );
 
-  itemsToLoad.forEach((result, index) => {
+  itemsToLoad.forEach((result) => {
     const resultItem = createResultItem(result);
     // Maak een 'div'-element voor de resultaten, alle resultaten weergaven kom hierin te staan
     const resultContainer = document.createElement("div");
     resultContainer.classList.add("searchResultsItems");
     resultContainer.appendChild(resultItem);//
     searchResults.appendChild(resultContainer);//
-    // resultItem.setAttribute('tabindex', currentIndex + index + 1); // Assign tabindex based on the current index
-    searchResults.appendChild(resultItem);
     loadMoreButton.style.display = "block";
   });
 
@@ -198,10 +143,7 @@ const handleSearchFormSubmit = async (event) => {
         }
         responseDataSet = JSON.parse(removeSpaceFromString(dirtySet));
 
-        localStorage.setItem(
-          "responseDataSet",
-          JSON.stringify(responseDataSet)
-        );
+        localStorage.setItem("responseDataSet", JSON.stringify(responseDataSet));
 
         // Load and display the items
         loadItems();
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,49 +1,42 @@
 const loading = document.querySelector(".loading");
 const resultsContainer = document.querySelector(".results-container");
 
-// Show loading animation
+// show loading animation
 export function showLoading() {
   loading.style.display = "block";
 }
 
-// Hide loading animation
+// hide loading animation
 export function hideLoading() {
   loading.style.display = "none";
 }
 
-// Handle UI states
+// WIP states
 export function uiState(typeState, param = "") {
   const ul = document.createElement("ul");
   const li = document.createElement("li");
 
+  // when something is wrong with data/api
   if (typeState === "noData") {
-    ul.textContent = "Oeps, er is iets fout gegaan. Ik kan de resultaten niet ophalen.";
+    ul.textContent = "Sorry, er ging iets mis. Kan resultaten niet ophalen.";
     resultsContainer.appendChild(ul);
     hideLoading();
-  } else if (typeState === "loading") {
+  }
+  // loading state
+  else if (typeState === "loading") {
     showLoading();
-  } else if (typeState === "hide") {
+  }
+  // show/hide results
+  else if (typeState === "hide") {
     if (param) {
       ul.style.display = "block";
       li.classList.add("speechOba");
-      li.textContent = `Ik heb resultaten gevonden voor ${param}`;
+      li.textContent = `Gevonden resultaten voor ${param}`;
       ul.appendChild(li);
       resultsContainer.appendChild(ul);
     } else {
       ul.style.display = "none";
     }
     hideLoading();
-  } else if (typeState === "noLink") {
-    ul.style.display = "block";
-    li.classList.add("speechOba");
-    li.textContent = `Sorry, het is niet mogelijk om de link te kopiëren.`;
-    ul.appendChild(li);
-    resultsContainer.appendChild(ul);
-  } else if (typeState === "copied") {
-    ul.style.display = "block";
-    li.classList.add("speechOba");
-    li.textContent = `De link is gekopieerd!`;
-    ul.appendChild(li);
-    resultsContainer.appendChild(ul);
   }
 }
