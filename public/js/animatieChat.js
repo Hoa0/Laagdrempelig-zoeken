874ccf8eb9949ac3d30e02e845a5d3ea8b51f7d2
hoa0 <t-hoa.l@hotmail.com>
2023-07-08 01:09:11 +0200

    update

--- public/js/animatieChat.js
+++ public/js/animatieChat.js
@@ -4,32 +4,30 @@ function animateGSAP() {
         return;
     }
 
-    const typing = document.getElementsByClassName("fallbackTyping");
+    const intro = document.getElementById("searchIntroduction")
     const categorieBtn = document.getElementById("speechBtnCateg");
-    const texts = document.getElementsByClassName("showResult");
     const nav = document.getElementsByTagName("header");
     const titleChat = document.getElementById("chatTitleContainer");
     const contentChat = document.getElementById("chat");
-    const showResults = document.getElementById("searchResults");
-    const speechInfoType = document.getElementById("speechInfoType");
-    const speechInfoChatOba = document.getElementById("speechInfoChatOba");
 
     // Hiding elements by default with CSS
-    gsap.set(typing, { opacity: 0 });
-    gsap.set(categorieBtn, { opacity: 0 });
-    gsap.set(speechInfoType, { opacity: 0 });
-    gsap.set(speechInfoChatOba, { opacity: 0 });
+    gsap.set(intro, {
+        opacity: 0
+    });
+    gsap.set(categorieBtn, {
+        opacity: 0
+    });
 
-    // Animatie for "typing" element
-    gsap.to(typing, {
+    // Animatie for "introduction search input" element
+    gsap.to(intro, {
         duration: 0.5,
         delay: 2,
         opacity: 1,
         onComplete: function () {
             // Na 4 seconden de onComplete-functie uitvoeren (blijven staan)
-            gsap.to(typing, {
+            gsap.to(intro, {
                 duration: 0.5,
-                delay: 2,
+                delay: 5,
                 opacity: 0,
                 display: "none",
             });
@@ -39,33 +37,26 @@ function animateGSAP() {
     // Animatie voor het "categorieBtn" element
     gsap.to(categorieBtn, {
         duration: 0.5,
-        delay: 6,
-        opacity: 1,
-    });
-
-    gsap.to(speechInfoType, {
-        duration: 0.5,
-        delay: 7,
-        opacity: 1,
-    });
-
-    gsap.to(speechInfoChatOba, {
-        duration: 0.5,
-        delay: 8,
-        opacity: 1,
-    });
-
-    gsap.to(showResults, {
-        duration: 0.5,
-        delay: 1,
+        delay: 2,
         opacity: 1,
     });
 
     function animation() {
-        gsap.from(nav, { duration: 1, y: 30, opacity: 0 });
-        gsap.from(titleChat, { duration: 1, y: 30, opacity: 0 });
-        gsap.from(contentChat, { duration: 1, y: 30, opacity: 0 });
-        gsap.from(showResults, { duration: 1, y: 30, opacity: 0 });
+        gsap.from(nav, {
+            duration: 1,
+            y: 30,
+            opacity: 0
+        });
+        gsap.from(titleChat, {
+            duration: 1,
+            y: 30,
+            opacity: 0
+        });
+        gsap.from(contentChat, {
+            duration: 1,
+            y: 30,
+            opacity: 0
+        });
     }
 
     animation();
